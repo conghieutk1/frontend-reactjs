b8c623a692cadba74d86318a3c8d79f8fdf34daa
Hieu Dang <hieu37.nghiadung@gmail.com>
2023-04-21 11:35:26 +0700

    21/03/2023

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter as Router } from "connected-react-router";
 import { history } from "../redux";
 import { ToastContainer } from "react-toastify";
-
 import {
     userIsAuthenticated,
     userIsNotAuthenticated,
@@ -19,7 +18,7 @@ import Header from "./Header/Header";
 import System from "../routes/System";
 
 import { CustomToastCloseButton } from "../components/CustomToast";
-import ConfirmModal from "../components/ConfirmModal";
+import HomePage from "./HomePage/HomePage.js";
 
 class App extends Component {
     handlePersistorState = () => {
@@ -45,7 +44,6 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
@@ -62,6 +60,10 @@ class App extends Component {
                                 <Route
                                     path={path.SYSTEM}
                                     component={userIsAuthenticated(System)}
+                                />
+                                <Route
+                                    path={path.HOMEPAGE}
+                                    component={HomePage}
                                 />
                             </Switch>
                         </span>
